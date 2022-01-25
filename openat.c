c0a1a2c12cb19f257d5991fd10849355badbd4e3
Jiri Zak <jiri.zak@bringauto.com>
2022-01-25 22:24:52 +0100

    add read and write to code

--- openat.c
+++ openat.c
@@ -1,7 +1,8 @@
 #include <linux/sched.h>
 
-BPF_PERF_OUTPUT(events);
-BPF_PERF_OUTPUT(try);
+BPF_PERF_OUTPUT(openat);
+BPF_PERF_OUTPUT(write);
+BPF_PERF_OUTPUT(read);
 
 struct data_t {
     u32 pid;
@@ -10,25 +11,70 @@ struct data_t {
 
 struct sys_enter_openat_t {
     uint64_t unused;
+
     u32 _nr;
     u64 dfd;
-    char * filename;
+    char *filename;
     u64 flags;
     u64 mode;
 };
 
-int sys_enter_opentat_fn(struct sys_enter_openat_t *args){
+int sys_enter_opentat_fn(struct sys_enter_openat_t *args) {
 
     struct data_t data = {};
     u32 pid = (u32)(bpf_get_current_pid_tgid() >> 32);
 
     data.pid = pid;
     bpf_probe_read_str(data.comm, TASK_COMM_LEN, args->filename);
-    bpf_trace_printk("openat works %d \\n", pid);
+    //bpf_trace_printk("Openat works %d \\n", pid);
 
-    events.perf_submit(args, &data, sizeof(data));
-    data.pid = 0;
-    try.perf_submit(args, &data, sizeof(data));
+    openat.perf_submit(args, &data, sizeof(data));
+
+    return 0;
+}
+
+
+struct sys_enter_write_t {
+    uint64_t unused;
+    u32 _nr;
+    u64 fd;
+    char *buf;
+    u64 count;
+};
+
+int sys_enter_write_fn(struct sys_enter_write_t *args){
+
+    struct data_t data = {};
+    u32 pid = (u32)(bpf_get_current_pid_tgid() >> 32);
+
+    data.pid = pid;
+    bpf_probe_read_str(data.comm, TASK_COMM_LEN, args->buf);
+    //bpf_trace_printk("Write works %d \\n", pid);
+
+    write.perf_submit(args, &data, sizeof(data));
+
+    return 0;
+}
+
+
+struct sys_enter_read_t {
+    uint64_t unused;
+    u32 _nr;
+    u64 fd;
+    char *buf;
+    u64 count;
+};
+
+int sys_enter_read_fn(struct sys_enter_read_t *args){
+
+    struct data_t data = {};
+    u32 pid = (u32)(bpf_get_current_pid_tgid() >> 32);
+
+    data.pid = pid;
+    bpf_probe_read_str(data.comm, TASK_COMM_LEN, args->buf);
+    //bpf_trace_printk("Read works %d \\n", pid);
+
+    read.perf_submit(args, &data, sizeof(data));
 
     return 0;
 }
--- openat.py
+++ openat.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
 
 from bcc import BPF
-from time import sleep
 
-bpf = BPF(src_file='openat.c')
-bpf.attach_tracepoint(tp="syscalls:sys_enter_openat", fn_name="sys_enter_opentat_fn")
+write_counter = 0
+read_counter = 0
 
 def handle_sys_enter_opanat(cpu, data, size):
-    data = bpf["events"].event(data)
-    print(f'Pid is {data.pid} and data are {data.comm}')
+    data = bpf["openat"].event(data)
+    print(f'Opentat pid is {data.pid} and data are {data.comm}')
 
-def handle_try(cpu, data, size):
-    data = bpf["events"].event(data)
-    print(f'TRY is {data.pid} and data are {data.comm}')
 
-bpf["events"].open_perf_buffer(handle_sys_enter_opanat)
-bpf["try"].open_perf_buffer(handle_try)
+def handle_sys_enter_write(cpu, data, size):
+    data = bpf["write"].event(data)
+    global write_counter
+    write_counter += 1
+    #print(f'TRY is {data.pid} and data are {data.comm}')
 
-while True:
-    try:
-        bpf.perf_buffer_poll()
-    except KeyboardInterrupt:
-        exit()
+
+def handle_sys_enter_read(cpu, data, size):
+    data = bpf["read"].event(data)
+    global read_counter
+    read_counter += 1
+    #print(f'Read pid is {data.pid} and data are {data.comm}')
+
+
+if __name__ == "__main__":
+    counter = 0
+    bpf = BPF(src_file='openat.c')
+    bpf.attach_tracepoint(tp="syscalls:sys_enter_openat", fn_name="sys_enter_opentat_fn")
+    bpf.attach_tracepoint(tp="syscalls:sys_enter_write", fn_name="sys_enter_write_fn")
+    bpf.attach_tracepoint(tp="syscalls:sys_enter_read", fn_name="sys_enter_read_fn")
+
+    bpf["openat"].open_perf_buffer(handle_sys_enter_opanat)
+    bpf["write"].open_perf_buffer(handle_sys_enter_write)
+    bpf["read"].open_perf_buffer(handle_sys_enter_read)
+
+    while True:
+        try:
+            bpf.perf_buffer_poll()
+        except KeyboardInterrupt:
+            print(f'Write = {write_counter}')
+            print(f'Read = {read_counter}')
+            exit()
